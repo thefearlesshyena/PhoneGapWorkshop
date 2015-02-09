0c120896990159f71bb21ddf81cfb36821f9f6cf
thefearlesshyena <luke@epimedia.com>
2015-02-09 14:11:32 -0700

    Added code for notifications

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 var app = {
 
+	initialize: function() {
+    var self = this;
+    this.store = new MemoryStore(function() {
+        self.showAlert('Store Initialized', 'Info');
+    });
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+	}
+	
+	showAlert: function (message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
+    }
+
     findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
